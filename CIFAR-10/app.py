a50c4d3864b02c198f67c88e88c41584fb04f91e
PorriDG3 <gn02053231@gmail.com>
2020-05-26 16:20:32 +0800

    cifar10

---
+++ CIFAR-10/app.py
@@ -0,0 +1,365 @@
+import os
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+from tensorflow import keras
+from tensorflow.keras import layers
+import tensorflow_datasets as tfds
+
+# 顯示TensorFlow Datasets目前提供的數據集
+tfds.list_builders()
+
+# 取得訓練數據，並順便讀取data的資訊
+train_data, info = tfds.load(
+    "cifar10", split='train[10%:]', with_info=True, data_dir='./tensorflow-datasets')
+# 取得驗證數據
+valid_data = tfds.load(
+    "cifar10", split='train[:10%]', data_dir='./tensorflow-datasets')
+# 取得測試數據
+test_data = tfds.load("cifar10", split=tfds.Split.TEST,
+                      data_dir='./tensorflow-datasets')
+
+# 顯示Cifar10數據集資訊
+print(info)
+
+# Cifar10的十個標籤
+labels_dict = dict(enumerate(info.features['label'].names))
+labels_dict
+
+# 建立一個dict用來計數每個類別標籤數量
+train_dict = {}
+# 讀取整個訓練資料集
+for data in train_data:
+    # 將讀取到的label轉乘numpy格式
+    label = data['label'].numpy()
+    # 以label沒dict的key，取出dict中的值+1，如果key在dict中沒有值預設為0
+    train_dict[label] = train_dict.setdefault(label, 0) + 1
+    # train_dict.setdefault(label, 0) + 1
+print(train_dict)
+
+# 建立一個顯示影像的陣列
+output = np.zeros((32 * 8, 32 * 8, 3), dtype=np.uint8)
+row = 0
+# 每一次取8比資料，共取8次，所以總共取得64比資料
+for data in train_data.batch(8).take(8):
+    # 將取得的8比堆疊起來，放入顯示影像陣列第N列中
+    output[:, row*32:(row+1)*32] = np.vstack(data['image'].numpy())
+    row += 1
+# 設定顯示視窗大小
+plt.figure(figsize=(8, 8))
+# 顯示影像
+plt.imshow(output)
+
+# 資料愈處理(Data Prepossessing ):
+# - 影像資料：將輸入資料做標準化，全部除以255，將像素值縮放到0~1之間。
+# - 標籤資料：做One-hot Encoding，例如類別2為[0, 0, 0, 0, 0, 0, 0, 0, 1, 0]。
+
+
+def parse_fn(dataset):
+    x = tf.cast(dataset['image'], tf.float32) / 255.
+    y = tf.one_hot(dataset['label'], 10)
+    return x, y
+
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE  # 自動調整模式
+batch_size = 64  # 批次大小
+train_num = int(info.splits['train'].num_examples / 10) * 9  # 訓練資料數量
+
+train_data = train_data.shuffle(train_num)  # 打散資料集
+# 載入前面的資料愈處理function，cpu數量為自動調整模式
+train_data = train_data.map(map_func=parse_fn, num_parallel_calls=AUTOTUNE)
+# 設定批次大小並將prefetch模式開啟(暫存空間為自動調整模式)。
+train_data = train_data.batch(batch_size).prefetch(buffer_size=AUTOTUNE)
+
+# 載入前面的資料愈處理function，cpu數量為自動調整模式
+valid_data = valid_data.map(map_func=parse_fn, num_parallel_calls=AUTOTUNE)
+# 設定批次大小並將prefetch模式開啟(暫存空間為自動調整模式)。
+valid_data = valid_data.batch(batch_size).prefetch(buffer_size=AUTOTUNE)
+
+# 載入前面的資料愈處理function，cpu數量為自動調整模式
+test_data = test_data.map(map_func=parse_fn, num_parallel_calls=AUTOTUNE)
+# 設定批次大小並將prefetch模式開啟(暫存空間為自動調整模式)。
+test_data = test_data.batch(batch_size).prefetch(buffer_size=AUTOTUNE)
+
+# 訓練Model-1（全連接神經網路）
+# 建立網路模型，這邊使用到以下幾種網路層：
+# - keras.Input：輸入層(輸入影像大小為32x32x3)
+# - layers.MaxPool2D：池化層(對特徵圖下採樣)
+# - layers.Flatten：扁平層(特徵圖轉成一維Tensor)
+# - layers.Dropout：Dropout層(每次訓練隨機丟棄30%網路)
+# - layers.Dense：全連接層(隱藏層使用ReLU激活函數，輸出層使用Softmax激活函數)
+inputs = keras.Input(shape=(32, 32, 3))
+x = layers.Flatten()(inputs)
+x = layers.Dense(128, activation='relu')(x)
+x = layers.Dense(256, activation='relu')(x)
+x = layers.Dense(512, activation='relu')(x)
+x = layers.Dense(512, activation='relu')(x)
+x = layers.Dropout(0.2)(x)
+x = layers.Dense(256, activation='relu')(x)
+x = layers.Dropout(0.2)(x)
+x = layers.Dense(64, activation='relu')(x)
+x = layers.Dropout(0.2)(x)
+outputs = layers.Dense(10, activation='softmax')(x)
+# 建立網路模型(將輸入到輸出所有經過的網路層連接起來)
+model_1 = keras.Model(inputs, outputs, name='model-1')
+model_1.summary()  # 顯示網路架構
+
+model_dir = 'lab4-logs/models'  # 設定儲存權重目錄
+if not os.path.exists(model_dir):
+    os.makedirs(model_dir)  # 創建儲存權重目錄
+
+# 將訓練記錄，存成TensorBoard的紀錄檔
+log_dir = os.path.join('lab4-logs', 'model-1')
+model_cbk = keras.callbacks.TensorBoard(log_dir='lab4-logs/model-1')
+# 儲存最好的網路模型權重
+model_mckp = keras.callbacks.ModelCheckpoint(model_dir + '/Best-model-1.hdf5', 
+                                             monitor='val_categorical_accuracy', 
+                                             save_best_only=True, 
+                                             mode='max')
+# 設定訓練使用的優化器、損失函數和指標函數
+model_1.compile(keras.optimizers.Adam(), 
+               loss=keras.losses.CategoricalCrossentropy(), 
+               metrics=[keras.metrics.CategoricalAccuracy()])
+
+# 訓練網路模型
+history_1 = model_1.fit(train_data,
+                        epochs=100, 
+                        validation_data=valid_data,
+                        callbacks=[model_cbk, model_mckp])
+
+# 驗證在測試集上
+loss, acc = model_1.evaluate(test_data)
+print('\nModel-1 Accuracy: {}%'.format(acc))
+
+# 訓練Model-2（卷積神經網路）
+# 建立網路模型，這邊使用到以下幾種網路層：
+# - keras.Input：輸入層(輸入影像大小為32x32x3)
+# - layers.Conv2D：卷積層(使用ReLU激活函數，以及3x3大小的kernel)
+# - layers.MaxPool2D：池化層(對特徵圖下採樣)
+# - layers.Flatten：扁平層(特徵圖轉成一維Tensor)
+# - layers.Dropout：Dropout層(每次訓練隨機丟棄50%網路)
+# - layers.Dense：全連接層(隱藏層使用ReLU激活函數，輸出層使用Softmax激活函數)
+inputs = keras.Input(shape=(32, 32, 3))
+x = layers.Conv2D(64, (3, 3), activation='relu')(inputs)
+x = layers.MaxPool2D()(x)
+x = layers.Conv2D(128, (3, 3), activation='relu')(x)
+x = layers.Conv2D(256, (3, 3), activation='relu')(x)
+x = layers.Conv2D(128, (3, 3), activation='relu')(x)
+x = layers.Conv2D(64, (3, 3), activation='relu')(x)
+x = layers.Flatten()(x)
+x = layers.Dense(64, activation='relu')(x)
+x = layers.Dropout(0.5)(x)
+outputs = layers.Dense(10, activation='softmax')(x)
+# 建立網路模型(將輸入到輸出所有經過的網路層連接起來)
+model_2 = keras.Model(inputs, outputs, name='model-2')
+model_2.summary()  # 顯示網路架構
+
+# 儲存訓練記錄檔
+log_dir = os.path.join('lab4-logs', 'model-2')
+model_cbk = keras.callbacks.TensorBoard(log_dir=log_dir)
+# 儲存最好的網路模型權重
+model_mckp = keras.callbacks.ModelCheckpoint(model_dir + '/Best-model-2.hdf5', 
+                                             monitor='val_categorical_accuracy', 
+                                             save_best_only=True, 
+                                             mode='max')
+
+# 設定訓練使用的優化器、損失函數和指標函數
+model_2.compile(keras.optimizers.Adam(), 
+               loss=keras.losses.CategoricalCrossentropy(), 
+               metrics=[keras.metrics.CategoricalAccuracy()])
+
+# 訓練網路模型
+history_2 = model_2.fit(train_data,
+                        epochs=100, 
+                        validation_data=valid_data,
+                        callbacks=[model_cbk, model_mckp])
+
+# 驗證在測試集上
+loss, acc = model_2.evaluate(test_data)
+print('\nModel-2 Accuracy: {}%'.format(acc))
+
+# 影像增強(Image Augmentation)
+# 取一張影像作為測試使用
+x = 3
+y = 7
+image_test = output[y*32:(y+1)*32, x*32:(x+1)*32, :]
+plt.imshow(image_test)
+
+# 水平翻轉：
+def flip(x):
+    """
+    flip image(翻轉影像)
+    """
+    x = tf.image.random_flip_left_right(x)  # 隨機左右翻轉影像
+    return x
+
+image_2 = flip(image_test)
+image = np.hstack((image_test, image_2))
+plt.imshow(image)
+
+# 顏色轉換：
+def color(x):
+    """
+     Color change(改變顏色)
+    """
+    x = tf.image.random_hue(x, 0.08)  # 隨機調整影像色調
+    x = tf.image.random_saturation(x, 0.6, 1.6)  # 隨機調整影像飽和度
+    x = tf.image.random_brightness(x, 0.05)  # 隨機調整影像亮度
+    x = tf.image.random_contrast(x, 0.7, 1.3)  # 隨機調整影像對比度
+    return x
+
+
+image_2 = color(image_test)
+image = np.hstack((image_test, image_2))
+plt.imshow(image)
+
+# 影像旋轉：
+def rotate(x):
+    """
+    Rotation image(影像旋轉)
+    """
+    # 隨機選轉n次(通過minval和maxval設定n的範圍)，每次選轉90度
+    x = tf.image.rot90(x, tf.random.uniform(shape=[], minval=1, maxval=4, dtype=tf.int32))
+    return x
+
+image_2 = rotate(image_test)
+image = np.hstack((image_test, image_2))
+plt.imshow(image)
+
+# 影像縮放：
+def zoom(x, scale_min=0.6, scale_max=1.4):
+    """
+    Zoom Image(影像縮放)
+    """
+    h, w, c = x.shape
+    scale = tf.random.uniform([], scale_min, scale_max)  # 隨機縮放比例
+    sh = h * scale  # 縮放後影像長度
+    sw = w * scale  # 縮放後影像寬度
+    x = tf.image.resize(x, (sh, sw))  # 影像縮放
+    x = tf.image.resize_with_crop_or_pad(x, h, w)  # 影像裁減和填補
+    return x
+
+image_2 = zoom(image_test)
+# 因為處理後影像會變成float型態，所以需轉換回來不然顯示時會出現問題
+image_2 = tf.cast(image_2, dtype=tf.uint8)
+image = np.hstack((image_test, image_2))  # 將原始圖和處理後水平合併
+plt.imshow(image)
+
+# Dataset 設定 (Augmentation)
+# 重新載入一次Dataset(因為前面已經對Dataset設定過了)
+train_data = tfds.load("cifar10", split='train[10%:]')
+
+# 資料愈處理(Data Prepossessing ):
+# -	標準化：將影像全部除以255，將像素值縮放到0~1之間。
+# -	影像增強：將影像水平翻轉、影像旋轉、顏色轉換和影像縮放。
+# -	標籤資料：做One-hot Encoding，例如類別2為[0, 0, 0, 0, 0, 0, 0, 0, 1, 0]。
+def parse_aug_fn(dataset):
+    """
+    Image Augmentation(影像增強) function
+    """
+    x = tf.cast(dataset['image'], tf.float32) / 255.  # 影像標準化
+    x = flip(x)  # 隨機水平翻轉
+    # 觸發顏色轉換機率50%
+    x = tf.cond(tf.random.uniform([], 0, 1) > 0.5, lambda: color(x), lambda: x)
+    # 觸發影像旋轉機率0.25%
+    x = tf.cond(tf.random.uniform([], 0, 1) > 0.75, lambda: rotate(x), lambda: x)
+    # 觸發影像縮放機率50%
+    x = tf.cond(tf.random.uniform([], 0, 1) > 0.5, lambda: zoom(x), lambda: x)
+    # 將輸出標籤轉乘One-hot編碼
+    y = tf.one_hot(dataset['label'], 10)
+    return x, y
+
+# Dataset設定：
+# 打散資料集
+train_data = train_data.shuffle(train_num)
+# 載入預處理「parse_aug_fn」function，cpu數量為自動調整模式
+train_data = train_data.map(map_func=parse_aug_fn, num_parallel_calls=AUTOTUNE)
+# 設定批次大小並將prefetch模式開啟（暫存空間為自動調整模式）
+train_data = train_data.batch(batch_size).prefetch(buffer_size=AUTOTUNE)
+
+# 測試Dataset經過Augmentation後的結果
+# 由於前面已經將train_data batch size設定為64，所以取一次資料就有64筆
+for images, labels in train_data.take(1):
+    images = images.numpy()
+# 建立一個顯示影像的陣列
+output = np.zeros((32 * 8, 32 * 8, 3))
+# 將64筆資料分別放入顯示影像的陣列
+for i in range(8):
+    for j in range(8):
+        output[i*32:(i+1)*32, j*32:(j+1)*32, :] = images[i*8+j]
+plt.figure(figsize=(8, 8))
+# 顯示影像
+plt.imshow(output)
+
+# 訓練Model-3（使用影增強方法訓練卷積神經網路）
+# 建立網路模型，這邊使用到以下幾種網路層：
+# - keras.Input：輸入層(輸入影像大小為32x32x3)
+# - layers.Conv2D：卷積層(使用ReLU激活函數，以及3x3大小的kernel)
+# - layers.MaxPool2D：池化層(對特徵圖下採樣)
+# - layers.Flatten：扁平層(特徵圖轉成一維Tensor)
+# - layers.Dropout：Dropout層(每次訓練隨機丟棄50%網路)
+# - layers.Dense：全連接層(隱藏層使用ReLU激活函數，輸出層使用Softmax激活函數)
+inputs = keras.Input(shape=(32, 32, 3))
+x = layers.Conv2D(64, (3, 3), activation='relu')(inputs)
+x = layers.MaxPool2D()(x)
+x = layers.Conv2D(128, (3, 3), activation='relu')(x)
+x = layers.Conv2D(256, (3, 3), activation='relu')(x)
+x = layers.Conv2D(128, (3, 3), activation='relu')(x)
+x = layers.Conv2D(64, (3, 3), activation='relu')(x)
+x = layers.Flatten()(x)
+x = layers.Dense(64, activation='relu')(x)
+x = layers.Dropout(0.5)(x)
+outputs = layers.Dense(10, activation='softmax')(x)
+# 建立網路模型(將輸入到輸出所有經過的網路層連接起來)
+model_3 = keras.Model(inputs, outputs, name='model-3')
+model_3.summary()  # 顯示網路架構
+
+# 儲存訓練記錄檔
+log_dir = os.path.join('lab4-logs', 'model-3')
+model_cbk = keras.callbacks.TensorBoard(log_dir=log_dir)
+# 儲存最好的網路模型權重
+model_mckp = keras.callbacks.ModelCheckpoint(model_dir + '/Best-model-3.hdf5', 
+                                             monitor='val_categorical_accuracy', 
+                                             save_best_only=True, 
+                                             mode='max')
+
+# 設定訓練使用的優化器、損失函數和指標函數
+model_3.compile(keras.optimizers.Adam(), 
+               loss=keras.losses.CategoricalCrossentropy(), 
+               metrics=[keras.metrics.CategoricalAccuracy()])
+
+# 訓練網路模型
+history_3 = model_3.fit(train_data,
+                        epochs=100, 
+                        validation_data=valid_data,
+                        callbacks=[model_cbk, model_mckp])
+
+# 驗證在測試集上：
+loss, acc = model_3.evaluate(test_data)
+print('\nModel-3 Accuracy: {}%'.format(acc))
+
+# 比較三種網路的訓練結果
+# 讀取各自最佳的網路權重：
+model_1.load_weights('lab4-logs/models/Best-model-1.hdf5')
+model_2.load_weights('lab4-logs/models/Best-model-2.hdf5')
+model_3.load_weights('lab4-logs/models/Best-model-3.hdf5')
+loss_1, acc_1 = model_1.evaluate(test_data)
+loss_2, acc_2 = model_2.evaluate(test_data)
+loss_3, acc_3 = model_3.evaluate(test_data)
+
+# 驗證在測試集數據上：
+loss_1, acc_1 = model_1.evaluate(test_data)
+loss_2, acc_2 = model_2.evaluate(test_data)
+loss_3, acc_3 = model_3.evaluate(test_data)
+
+# 顯示預測的損失值與準確度結果：
+loss = [loss_1, loss_2, loss_3]  
+acc = [acc_1, acc_2, acc_3]
+
+dict = {"Loss": loss,  
+        "Accuracy": acc}
+
+df = pd.DataFrame(dict)
+df
